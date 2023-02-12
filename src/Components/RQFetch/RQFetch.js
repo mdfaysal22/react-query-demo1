29938e28c08f5e6f06842bae35a6e72fda0477b7
Faysal Developer <apple@MacBook-Pro.local>
2023-02-12 07:22:57 +0600

    CacheTime

--- src/Components/RQFetch/RQFetch.js
+++ src/Components/RQFetch/RQFetch.js
@@ -1,32 +1,44 @@
-import axios from 'axios';
-import React from 'react';
-import { useQuery } from 'react-query';
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
 
 const RQFetch = () => {
-    const dataFetching = () => {
-        return axios.get('http://localhost:4000/allPosts')
-     }
-    const {data, isLoading, isError, error} = useQuery(["allPosts"],dataFetching)
+  const dataFetching = () => {
+    return axios.get("http://localhost:4000/allPosts");
+  };
 
-    if(isLoading) {
-        return <h2>Loading...</h2>
+  /**
+   * React Query accept 3 argument
+   * 01. Unique Key , accept array
+   * 02. a array function to fetching data -> () => {}
+   * 03. Accept a Object -> this object accept some property like cacheTime : 5000 (ms),
+   */
+
+  const { data, isLoading,isFetching, isError, error } = useQuery(
+    ["allPosts"],
+    dataFetching,
+    {
+        cacheTime: 5000,
     }
-    if(isError) {
-        return <h2>{error.message}</h2>
-    }
-    return (
-        <div>
-            <h2>RQ data Fetching</h2>
-            {
-                data?.data.map(post => 
-                    <div key={post.id}>
-                        <h2>{post.author}</h2>
-                        <p>{post.post}</p>
-                    </div>
-                )
-            }
+  );
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+  return (
+    <div>
+      <h2>RQ data Fetching</h2>
+      {data?.data.map((post) => (
+        <div key={post.id}>
+          <h2>{post.author}</h2>
+          <p>{post.post}</p>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default RQFetch;
